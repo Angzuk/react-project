eddaa47fc1fc8632e08339876cab286c12384624
krawczuk92@gmail.com <141040688+Angzuk@users.noreply.github.com>
2024-11-25 21:52:01 +0000

    Working with lists with dynamic data

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 
-function Header({ name, year}) {
+function Header({ name, year }) {
   return (
     <header>
       <h1>{name}'s Kitchen</h1>
@@ -9,13 +9,27 @@ function Header({ name, year}) {
   );
 }
 
+const items = [
+  "Macarioni and Cheese",
+  "Salmon with Potatos",
+  "Tofu with Vegetables",
+];
+
+function Main({ dishes }) {
+  return (
+    <ul>
+      {dishes.map((dish) => (
+        <li style={{ listStyleType:"None" }}>{dish}</li>
+      ))}
+    </ul>
+  );
+}
+
 function App() {
   return (
     <div>
-      <Header name = "Alex" year ={new Date().getFullYear()}/>
-      <main>
-        <h2>We serve the most delicious food around</h2>
-      </main>
+      <Header name="Alex" year={new Date().getFullYear()} />
+      <Main dishes={items} />
     </div>
   );
 }
