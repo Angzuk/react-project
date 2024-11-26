e19e98c6ccf9e8e1a2e683e9e59331a4d5ebd52b
krawczuk92@gmail.com <141040688+Angzuk@users.noreply.github.com>
2024-11-26 12:39:25 +0000

    Understanding useState hook

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,5 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-const [firstAnimal, second , third] = ["giraffe","zebra","bear"]; // can be also represented: [, , third]
-console.log(second);
-
-createRoot(document.getElementById('root')).render(<App />);
+createRoot(document.getElementById("root")).render(<App />);
